4329c11978370de1286a715bf1e0c7770d37f318
Sai Saran Vaidyanathan <saisaranvaidyanathan@ITs-MacBook-Pro-94.local>
2017-05-30 15:46:16 -0700

    Minor changes

--- setup/run-image.sh
+++ setup/run-image.sh
@@ -1,34 +1,19 @@
 : '
 Variable definitions:
 
-    APIGEE_ORG:
+    org:
         Name of the Apigee org you want your proxy to be deployed in. Used by
         maven during proxy deployment.
-    APIGEE_USERNAME
+    username
         Username of the user to be used for proxy deployment. Used by maven
         during proxy deployment.
-    APIGEE_PASSWORD
+    password
         Password of the user to be used for proxy deployment. Used by maven
         during proxy deployment.
-    API_DOMAIN_TEST
-        Apigee proxy domain name with port (excluding scheme) for test
-        environment. Used by proxy integration testing.
-        Example: oseymen-test.apigee.net
-    API_DOMAIN_PROD
-        Apigee proxy domain name with port (excluding scheme) for prod
-        environment. Used by proxy integration testing.
-        Example: oseymen-prod.apigee.net
-    SCM_URL
-        Git remote URL to the repo. Used by Jenkins to clone and fetch proxy
-        source code.
-        Example: git@github.com:seymen/currency-v1.git
 '
 
 docker run -d -p 9001:8080 \
-	-e APIGEE_ORG=. \
-	-e APIGEE_USERNAME=. \
-	-e APIGEE_PASSWORD=. \
-	-e API_DOMAIN_TEST=. \
-	-e API_DOMAIN_PROD=. \
-	-e SCM_URL=. \
+	-e org=. \
+	-e username=. \
+	-e password=. \
 --name my-ci apigee/ci
